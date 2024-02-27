0614f95ca171a430e72c7e83f8b80e648cdc6561
pragnavi <pragnavi1398@gmail.com>
2024-02-26 22:36:02 -0500

    Created Front Page

---
+++ Wordle/DO_NOT_MODIFY/FrontPageController.swift
@@ -0,0 +1,23 @@
+//
+//  FrontPageController.swift
+//  Wordle
+//
+//  Created by Pragnavi Ravuluri Sai Durga on 2/26/24.
+//
+
+import UIKit
+
+class FrontPageController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    @IBOutlet weak var appNameLabel: UILabel!
+    
+    @IBOutlet weak var appLogoImage: UIImageView!
+    @IBOutlet weak var playButton: UIButton!
+}
